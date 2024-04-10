c9e077f31cc5920ae9d9e026e7d81326488c48a0
WilliamsSong27 <songwilliams44@gmail.com>
2024-04-09 19:16:55 -0600

    Completed Lab 6a

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 50.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move gameObject forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
